33124bebc3ddb4cf2bcb4112be0994570e42e268
titouan-aclr <122918576+titouan-aclr@users.noreply.github.com>
2024-02-16 10:47:15 +0100

    define mainActivity structure

--- app/src/main/java/org/imt/nordeurope/titouanauclair/tp_leboncoin/MainActivity.java
+++ app/src/main/java/org/imt/nordeurope/titouanauclair/tp_leboncoin/MainActivity.java
@@ -3,6 +3,12 @@ package org.imt.nordeurope.titouanauclair.tp_leboncoin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickList(View view){
+        //Intent lancementSecondeActivite = new Intent(MainActivity.this , SecondActivity . class );
+        Log.d("Button click", "Clic sur list");
+        //startActivity (lancementSecondeActivite);
+    }
+
+    public void onClickAdd(View view){
+        //Intent lancementSecondeActivite = new Intent(MainActivity.this , SecondActivity . class );
+        Log.d("Button click", "Clic sur add");
+        //startActivity (lancementSecondeActivite);
+    }
+
 }
